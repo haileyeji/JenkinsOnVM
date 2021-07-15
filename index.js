12bab105234fbf9bd925eb512a4fb927d7f81227
hailey <ruizhily@gmail.com>
2021-07-15 13:35:41 +0900

    8th

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I like vitamins!');
+    res.send('I like vitamins & minerals!');
 });
 
 app.get('/start', function(req, res){
