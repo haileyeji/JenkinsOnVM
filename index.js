d1698f67dc8930f39b6ae08f75af4bcd695e6d4e
hailey <ruizhily@gmail.com>
2021-07-15 11:50:51 +0900

    2nd

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I like Hailey!');
+    res.send('I like Hye!');
 });
 
 app.get('/start', function(req, res){
