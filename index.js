59351f2fe29ea7ce205a809b4fb7c4f9c90a63ca
hailey <ruizhily@gmail.com>
2021-07-15 16:09:41 +0900

    test1

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('Bonjour!');
+    res.send('TEST1!');
 });
 
 app.get('/start', function(req, res){
