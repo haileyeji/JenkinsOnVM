57849e39306d7328b668e91fbe6232b243ccd5b3
hailey <ruizhily@gmail.com>
2021-07-15 13:47:27 +0900

    bonjour

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I like test2!');
+    res.send('Bonjour!');
 });
 
 app.get('/start', function(req, res){
